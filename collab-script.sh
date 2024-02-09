0b424ba6f494b825cd303cb24a983737778b4acf
alex snow <alexsnow348@gmail.com>
2024-02-09 19:55:36 +0100

    updated to test with script

--- collab-script.sh
+++ collab-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git config --global user.email "alexsnow348@gmail.com"
-git config --global user.name "alex snow collab"
+git config --global user.name "alex snow"
 git pull origin alex-main
 git status
 git add .
