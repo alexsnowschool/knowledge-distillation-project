5562e91f4060aa0e3576827bd5a97168b0178c77
Alex Snow <alexsnow348@gmail.com>
2024-11-15 18:39:14 +0100

    updated for run_selected script

--- src/SimKD/scripts/run_selected.sh
+++ src/SimKD/scripts/run_selected.sh
@@ -19,17 +19,16 @@ commands=(
 
 # similarity
 "python train_student.py --path_t ../../experiment_artifacts/saved_model_artifacts/teachers/models/resnet32x4_vanilla_cifar100/resnet32x4_best.pth --epochs 240 --learning_rate 0.01  --distill similarity --model_s MobileNetV2_1_0 -c 0 -d 0 -b 1 --trial 0 --gpu_id 0"
-# "python train_student.py --path_t ./save/teachers/models/resnet38x2_vanilla_cifar100_trial_1/resnet38x2_best.pth --epochs 240 --learning_rate 0.05 --distill similarity --model_s resnet8x4 -c 1 -d 1 -b 0 --trial 0 --gpu_id 0"
-# "python train_student.py --path_t ./save/teachers/models/resnet38x2_vanilla_cifar100_trial_1/resnet38x2_best.pth --epochs 240 --learning_rate 0.001 --distill similarity --model_s resnet8x4 -c 1 -d 1 -b 0 --trial 0 --gpu_id 0"
+"python train_student.py --path_t ../../experiment_artifacts/saved_model_artifacts/teachers/models/resnet32x4_vanilla_cifar100/resnet32x4_best.pth --epochs 240 --learning_rate 0.05  --distill similarity --model_s MobileNetV2_1_0 -c 0 -d 0 -b 1 --trial 0 --gpu_id 0"
+"python train_student.py --path_t ../../experiment_artifacts/saved_model_artifacts/teachers/models/resnet32x4_vanilla_cifar100/resnet32x4_best.pth --epochs 240 --learning_rate 0.001  --distill similarity --model_s MobileNetV2_1_0 -c 0 -d 0 -b 1 --trial 0 --gpu_id 0"
 
-# "python train_student.py --path_t ./save/teachers/models/resnet38x2_vanilla_cifar100_trial_1/resnet38x2_best.pth --epochs 240 --learning_rate 0.01 --distill similarity --model_s resnet8x4 -c 1 -d 1 -b 0 --trial 1 --gpu_id 0"
-# "python train_student.py --path_t ./save/teachers/models/resnet38x2_vanilla_cifar100_trial_1/resnet38x2_best.pth --epochs 240 --learning_rate 0.05 --distill similarity --model_s resnet8x4 -c 1 -d 1 -b 0 --trial 1 --gpu_id 0"
-# "python train_student.py --path_t ./save/teachers/models/resnet38x2_vanilla_cifar100_trial_1/resnet38x2_best.pth --epochs 240 --learning_rate 0.001 --distill similarity --model_s resnet8x4 -c 1 -d 1 -b 0 --trial 1 --gpu_id 0"
+"python train_student.py --path_t ../../experiment_artifacts/saved_model_artifacts/teachers/models/resnet32x4_vanilla_cifar100/resnet32x4_best.pth --epochs 240 --learning_rate 0.01  --distill similarity --model_s MobileNetV2_1_0 -c 0 -d 0 -b 1 --trial 1 --gpu_id 0"
+"python train_student.py --path_t ../../experiment_artifacts/saved_model_artifacts/teachers/models/resnet32x4_vanilla_cifar100/resnet32x4_best.pth --epochs 240 --learning_rate 0.05  --distill similarity --model_s MobileNetV2_1_0 -c 0 -d 0 -b 1 --trial 1 --gpu_id 0"
+"python train_student.py --path_t ../../experiment_artifacts/saved_model_artifacts/teachers/models/resnet32x4_vanilla_cifar100/resnet32x4_best.pth --epochs 240 --learning_rate 0.001  --distill similarity --model_s MobileNetV2_1_0 -c 0 -d 0 -b 1 --trial 1 --gpu_id 0"
 
-# "python train_student.py --path_t ./save/teachers/models/resnet38x2_vanilla_cifar100_trial_1/resnet38x2_best.pth --epochs 240 --learning_rate 0.01 --distill similarity --model_s resnet8x4 -c 1 -d 1 -b 0 --trial 2 --gpu_id 0"
-# "python train_student.py --path_t ./save/teachers/models/resnet38x2_vanilla_cifar100_trial_1/resnet38x2_best.pth --epochs 240 --learning_rate 0.05 --distill similarity --model_s resnet8x4 -c 1 -d 1 -b 0 --trial 2 --gpu_id 0"
-# "python train_student.py --path_t ./save/teachers/models/resnet38x2_vanilla_cifar100_trial_1/resnet38x2_best.pth --epochs 240 --learning_rate 0.001 --distill similarity --model_s resnet8x4 -c 1 -d 1 -b 0 --trial 2 --gpu_id 0"
-
+"python train_student.py --path_t ../../experiment_artifacts/saved_model_artifacts/teachers/models/resnet32x4_vanilla_cifar100/resnet32x4_best.pth --epochs 240 --learning_rate 0.01  --distill similarity --model_s MobileNetV2_1_0 -c 0 -d 0 -b 1 --trial 2 --gpu_id 0"
+"python train_student.py --path_t ../../experiment_artifacts/saved_model_artifacts/teachers/models/resnet32x4_vanilla_cifar100/resnet32x4_best.pth --epochs 240 --learning_rate 0.05  --distill similarity --model_s MobileNetV2_1_0 -c 0 -d 0 -b 1 --trial 2 --gpu_id 0"
+"python train_student.py --path_t ../../experiment_artifacts/saved_model_artifacts/teachers/models/resnet32x4_vanilla_cifar100/resnet32x4_best.pth --epochs 240 --learning_rate 0.001  --distill similarity --model_s MobileNetV2_1_0 -c 0 -d 0 -b 1 --trial 2 --gpu_id 0"
 )
 
 # Loop over the commands array
